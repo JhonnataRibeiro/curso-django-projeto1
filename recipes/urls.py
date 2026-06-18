653e6460d2200cb9f0c85aaef337a019091be7d8
JhonnataRibeiro <jon96ribeiro@gmail.com>
2026-06-18 11:25:12 -0300

    Atualizando

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from recipes.views import home
+# from recipes.views import home
+from recipes import views
 
 urlpatterns = [
-    path('', home),
+    # path('', home),
+    path('', views.home, name="recipes-home"),
+    path('recipes/<int:id>/', views.recipe, name="recipes-recipe"),
+    path('recipes/category/<int:category_id>/', views.recipe, name="category"),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from utils.recipes.factory import make_recipe
+from recipes.models import Recipe
 
 def home(request):
+    recipes = Recipe.objects.all().order_by('-id')
     return render(request, "recipes/pages/home.html", context={
-        "name": "Jhonnata Ribeiro"
+        "recipes": recipes
+    })
+
+def category(request, category_id):
+    recipes = Recipe.objects.filter(category_id=category_id).order_by('-id')
+    return render(request, "recipes/pages/home.html", views.category, context={
+        "recipes": recipes
+    })
+
+def recipe(request, id):
+    return render(request, "recipes/pages/recipe-view.html", context={
+        "recipe": "make_recipe()",
+        "is_detail_page": True,
     })
